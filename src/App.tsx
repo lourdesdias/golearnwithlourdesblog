842a4b792cd8a250d9f266e22ba1d7968f399844
lourdesdias <lourdes.dias.invest@gmail.com>
2026-02-06 22:32:16 -0700

    Trigger deployment with HashRouter fix

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+// Final deployment trigger for go.learnwithlourdes.blog
 import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import TravelWebinar from "./pages/TravelWebinar";
