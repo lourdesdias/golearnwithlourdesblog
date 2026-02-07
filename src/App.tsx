a327ac45dd6e34156fe93190d76f49b0a039c85d
lourdesdias <lourdes.dias.invest@gmail.com>
2026-02-06 21:24:13 -0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { HashRouter, Routes, Route, Navigate } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Index from "./pages/Index";
 import TravelWebinar from "./pages/TravelWebinar";
 import PrivacyPolicy from "./pages/PrivacyPolicy";
@@ -21,28 +21,21 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <HashRouter>
+      <BrowserRouter>
         <Routes>
-          {/* Home */}
+          <Route path="/" element={<LinkInBio />} />
           <Route path="/" element={<Index />} />
-
-          {/* Legacy path (hash-based) */}
-          <Route path="/travelfreedom" element={<Navigate to="/" replace />} />
-
-          {/* Other pages */}
-          <Route path="/linkinbio" element={<LinkInBio />} />
-          <Route path="/travelwebinar" element={<TravelWebinar />} />
+          <Route path="/" element={<TravelWebinar />} />
           <Route path="/visionarchitect" element={<VisionArchitect />} />
           <Route path="/viralengine" element={<ViralEngine />} />
           <Route path="/newsletter" element={<Newsletter />} />
           <Route path="/privacy-policy" element={<PrivacyPolicy />} />
           <Route path="/terms-of-service" element={<TermsOfService />} />
           <Route path="/income-disclosure" element={<IncomeDisclosure />} />
-
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
-      </HashRouter>
+      </BrowserRouter>
     </TooltipProvider>
   </QueryClientProvider>
 );
